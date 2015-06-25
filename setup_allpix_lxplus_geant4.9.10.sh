629aea1397da16a8fdc7e7171a6b6db202a425e0
mathieubenoit <mathieu.benoit@cern.ch>
2015-06-25 16:38:40 +0200

    added a setup script to use geant4.9.10 on lxplus, with openInventor and such

---
+++ setup_allpix_lxplus_geant4.9.10.sh
@@ -0,0 +1,38 @@
+
+
+#gcc4.8
+source /afs/cern.ch/sw/lcg/external/gcc/4.8/x86_64-slc6/setup.sh
+
+
+#python2.7.4
+export PATH="/afs/cern.ch/sw/lcg/external/Python/2.7.4/x86_64-slc6-gcc48-opt/bin:$PATH"
+export LD_LIBRARY_PATH="/afs/cern.ch/sw/lcg/external/Python/2.7.4/x86_64-slc6-gcc48-opt/lib:$LD_LIBRARY_PATH" 
+
+
+#numpy/scipy/sympy
+
+export PYTHONPATH=$PYTHONPATH:/afs/cern.ch/eng/clic/TBData/software/ROOT6_gcc48_python2.7/pytools/numpy/lib/python2.7/site-packages
+export PYTHONPATH=$PYTHONPATH:/afs/cern.ch/eng/clic/TBData/software/ROOT6_gcc48_python2.7/pytools/scipy/lib/python2.7/site-packages
+export PYTHONPATH=$PYTHONPATH:/afs/cern.ch/eng/clic/TBData/software/ROOT6_gcc48_python2.7/pytools/sympy/lib/python2.7/site-packages
+export PYTHONPATH=$PYTHONPATH:/afs/cern.ch/eng/clic/TBData/software/ROOT6_gcc48_python2.7/pytools/cython/lib/python2.7/site-packages
+export PYTHONPATH=$PYTHONPATH:/afs/cern.ch/eng/clic/TBData/software/ROOT6_gcc48_python2.7/pytools/fastcluster/lib/python2.7/site-packages
+
+
+#root6
+source /afs/cern.ch/eng/clic/TBData/software/ROOT6_gcc48_python2.7/root/bin/thisroot.sh
+
+
+#xerces 
+
+export LD_LIBRARY_PATH=/afs/cern.ch/eng/clic/software/Pixel_TestBeam_Software/xerces/lib:$LD_LIBRARY_PATH
+
+
+#open Inventor
+export LD_RUN_PATH=/afs/cern.ch/eng/clic/software/Pixel_TestBeam_Software/OpenInventor/OIV_Install/lib
+export PATH=$PATH:/afs/cern.ch/eng/clic/software/Pixel_TestBeam_Software/OpenInventor/OIV_Install/bin
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/afs/cern.ch/eng/clic/software/Pixel_TestBeam_Software/OpenInventor/OIV_Install/lib
+export OIVHOME=/afs/cern.ch/eng/clic/software/Pixel_TestBeam_Software/OpenInventor/OIV_Install
+export OIVLIBS=/afs/cern.ch/eng/clic/software/Pixel_TestBeam_Software/OpenInventor/OIV_Install/lib/libSoXt.so
+
+
+source /afs/cern.ch/eng/clic/TBData/software/ROOT6_gcc48_python2.7/geant4/4.9.10.1p02/share/Geant4-10.1.2/geant4make/geant4make.sh
