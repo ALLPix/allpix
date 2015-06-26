81aa203ad4969aa27f1ae620aa352b68f4b3a80c
mathieubenoit <mathieu.benoit@cern.ch>
2015-06-26 10:46:51 +0200

    Changed root version for lxplus+geant4.9.10 script so it does not throw runtime warning about slightly different gcc

--- setup_allpix_lxplus_geant4.9.10.sh
+++ setup_allpix_lxplus_geant4.9.10.sh
@@ -19,7 +19,7 @@ export PYTHONPATH=$PYTHONPATH:/afs/cern.ch/eng/clic/TBData/software/ROOT6_gcc48_
 
 
 #root6
-source /afs/cern.ch/eng/clic/TBData/software/ROOT6_gcc48_python2.7/root/bin/thisroot.sh
+source /afs/cern.ch/eng/clic/TBData/software/ROOT6_gcc48_python2.7/geant4/root-6.04.00/bin/thisroot.sh
 
 
 #xerces 
--- src/SelDict.cc
+++ src/SelDict.cc
@@ -1109,8 +1109,8 @@ namespace {
     };
     static const char* includePaths[] = {
 "./include",
-"/home/mbenoit/workspace/GEANT4/root-6.04.00/include",
-"/home/mbenoit/workspace/allpix/",
+"/afs/cern.ch/eng/clic/TBData/software/ROOT6_gcc48_python2.7/geant4/root-6.04.00/include",
+"/afs/cern.ch/user/m/mbenoit/tmp/allpix/",
 0
     };
     static const char* fwdDeclCode = 
