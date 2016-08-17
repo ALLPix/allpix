72de145ba9505030fa10528b2ffdf2b64a6ba833
Kilian Lohmann <kilian.lohmann@phys.uni-goettingen.de>
2016-08-17 17:03:12 +0200

    changed hit file readout to only output the median hit to the lcio file

--- share/EUTelConverter/RootFileConverter.cc
+++ share/EUTelConverter/RootFileConverter.cc
@@ -4,6 +4,7 @@
 #include <cmath>
 #include <vector>
 #include <string>
+#include <array>
 #include <map>
 
 #include <TROOT.h>
@@ -27,6 +28,31 @@
 
 using namespace lcio;
 
+/*struct TrueHit {
+
+	double edep_total;
+	double sensor_ID;
+
+	std::vector<double*> pos;
+	std::vector<int> pdgId;
+	std::vector<float> edep;
+
+	~TrueHit() {
+
+		for (unsigned int i = 0; i < pos.size(); i++) delete [] pos[i];
+	}
+};
+
+struct Event {
+
+	std::vector<TrueHit*> hits;
+
+	~Event() {
+
+		for (unsigned int i = 0; i < hits.size(); i++) delete hits[i];
+	}
+};*/
+
 void print_info();
 
 int main(int argc, char* argv[]) {
@@ -35,7 +61,7 @@ int main(int argc, char* argv[]) {
 	int n_sensors, n_DUTs;
 	std::vector<int> DUT_IDs;
 
-	if (argc < 3) {
+	if (argc < 4) {
 
 		print_info();//if not enough arguments are given, print user info
 		return 0;
@@ -51,7 +77,7 @@ int main(int argc, char* argv[]) {
 			for (int i = 0; i < 4-argc; i++) {
 
 				int temp = 0;
-				sscanf(argv[3+i], "%d", &temp);
+				sscanf(argv[4+i], "%d", &temp);
 				DUT_IDs.push_back(temp);
 			}
 		}
@@ -104,19 +130,29 @@ int main(int argc, char* argv[]) {
 		frame_trees[i+n_sensors] = (TTree*) frame_files[i+n_sensors]->Get("MPXTree");
 	}
 
+	FrameStruct** root_frames = new FrameStruct*[n_sensors+n_DUTs];
+	SimpleHits** root_hits = new SimpleHits*[n_sensors+n_DUTs];
+	for (int i = 0; i < n_sensors+n_DUTs; i++) {
+
+		root_frames[i] = NULL;
+		root_hits[i] = NULL;
+
+		frame_trees[i]->SetBranchAddress("FramesData", &root_frames[i]);
+		true_hit_trees[i]->SetBranchAddress("SimpleHits", &root_hits[i]);
+
+	}
+
 	int n_entries = true_hit_trees[0]->GetEntries();
-
-	FrameStruct* root_frame = NULL;
-	SimpleHits* root_hit = NULL;
-	true_hit_trees[0]->SetBranchAddress("SimpleHits", &root_hit);		
 	true_hit_trees[0]->GetEntry(n_entries-1);
-	int n_runs = root_hit->run;
-	int run_number = 0;
-	std::vector<int> sensor_hit_incrs(n_sensors);
-	for (int i = 0; i < n_sensors; i++) sensor_hit_incrs[i] = 0;
-	std::vector<int> DUT_hit_incrs(n_DUTs);
-	for (int i = 0; i < n_DUTs; i++) DUT_hit_incrs[i] = 0;
+	int n_runs = root_hits[0]->run;
 
+	/*Event** hit_events = new Event*[n_runs];
+	for (int i = 0; i < n_runs; i++) hit_events[i] = new Event();*/
+
+	std::vector<int> hit_incrs(n_sensors+n_DUTs);
+	for (int i = 0; i < n_sensors+n_DUTs; i++) hit_incrs[i] = 0;
+
+	int run_number = 0;
 	std::string detector_name = "EUTelescope";
 
 	LCWriter* writer = LCFactory::getInstance()->createLCWriter();
@@ -139,7 +175,7 @@ int main(int argc, char* argv[]) {
 	run_header->parameters().setValue  ("DateTime"        , "24.12.2000  23:59:59.000000000");
 	run_header->parameters().setValue  ("EUDRBDet"        , "MIMOSA26");
 	run_header->parameters().setValue  ("EUDRBMode"       , "ZS2");
-   	writer->writeRunHeader(run_header);
+	writer->writeRunHeader(run_header);
 	delete run_header;
 
 	for (int i = 0; i < n_runs; i++) {
@@ -177,6 +213,7 @@ int main(int argc, char* argv[]) {
 		//Telescope data collection
 		LCCollectionVec* sensor_hit_coll = new LCCollectionVec(LCIO::TRACKERHIT);
 		LCCollectionVec* sensor_frame_coll = new LCCollectionVec(LCIO::TRACKERDATA);
+		//LCCollectionVec* sensor_vertex_coll = new LCCollectionVec(LCIO::TRACKERHIT);
 		CellIDEncoder<TrackerDataImpl> frame_encoder_sensor(encoding_string, sensor_frame_coll);
 
 		//fil telescope collection
@@ -184,9 +221,7 @@ int main(int argc, char* argv[]) {
 
 			TrackerDataImpl* frame_data = new TrackerDataImpl();
 
-			frame_trees[j]->SetBranchAddress("FramesData", &root_frame);
 			frame_trees[j]->GetEntry(i);
-			true_hit_trees[j]->SetBranchAddress("SimpleHits", &root_hit);
 
 			frame_encoder_sensor.reset();
 			frame_encoder_sensor["sensorID"] = j;
@@ -197,37 +232,51 @@ int main(int argc, char* argv[]) {
 			//there are multiple entries with the same run (aka event) number,
 			//hence must loop over all the entries with the same run number as
 			//the first for loop (looping over run numbers)
-			while (true_hit_trees[j]->GetEntry(sensor_hit_incrs[j]) and root_hit->run == i) {
+			while (true_hit_trees[j]->GetEntry(hit_incrs[j]) and root_hits[j]->run == i) {
+
+				/*hit_events[i]->hits.push_back(new TrueHit());
+				hit_events[i]->hits.back()->sensor_ID = 300+j;
+				hit_events[i]->hits.back()->edep_total = root_hits[j]->edepTotal;
+				hit_events[i]->hits.back()->pdgId = root_hits[j]->pdgId;
+				hit_events[i]->hits.back()->edep = root_hits[j]->edep;*/
 
 				double* hit_pos = new double[3];
-				for (unsigned int k = 0; k < root_hit->pos.size(); k++) {
+				for (unsigned int k = 0; k < root_hits[j]->pos.size(); k++) {
 
-					TrackerHitImpl* true_hit_data = new TrackerHitImpl();
+					//hit_events[i]->hits.back()->pos.push_back(hit_pos);
 
-					hit_pos[0] = root_hit->pos[k].X();
-					hit_pos[1] = root_hit->pos[k].Y();
-					hit_pos[2] = root_hit->pos[k].Z();
+					if (k == root_hits[j]->pos.size()/2 - 1) {
 
-					true_hit_data->setCellID0(300+j);
-					true_hit_data->setType(sensor_hit_incrs[j]);
-					true_hit_data->setPosition(hit_pos);
-					true_hit_data->setEDep(root_hit->edep[k]);
+						TrackerHitImpl* true_hit_data = new TrackerHitImpl();
 
-					sensor_hit_coll->addElement(true_hit_data);
+						hit_pos[0] = root_hits[j]->pos[k].X();
+						hit_pos[1] = root_hits[j]->pos[k].Y();
+						hit_pos[2] = root_hits[j]->pos[k].Z();
+
+						true_hit_data->setCellID0(300+j);
+						true_hit_data->setQuality(root_hits[j]->event);
+						true_hit_data->setType(root_hits[j]->pdgId[k]);
+						true_hit_data->setPosition(hit_pos);
+						true_hit_data->setEDep(root_hits[j]->edepTotal);
+
+						sensor_hit_coll->addElement(true_hit_data);
+
+					}
 				}
+				//if ((i%10 == 0) || (i == 0)) cout << root_hits[j]->event << endl;
 
-				sensor_hit_incrs[j]++;
+				hit_incrs[j]++;
 
 				delete [] hit_pos;
 			}
 
 			//loop over hits from frame
 			std::vector<float> charge_vec;
-			for (auto it = root_frame->GetHitMap().begin(); it != root_frame->GetHitMap().end(); ++it) {
+			for (auto it = root_frames[j]->GetHitMap().begin(); it != root_frames[j]->GetHitMap().end(); ++it) {
 
 				//the key contains position of hit in the format key = y*width + x
-				float x = (it->first)%root_frame->GetWidth();
-				float y = (it->first)/root_frame->GetWidth();
+				float x = (it->first)%root_frames[j]->GetWidth();
+				float y = (it->first)/root_frames[j]->GetWidth();
 				float TOT = it->second;
 
 				charge_vec.push_back(x);
@@ -239,60 +288,99 @@ int main(int argc, char* argv[]) {
 			frame_data->setChargeValues(charge_vec);
 			sensor_frame_coll->addElement(frame_data);
 			charge_vec.clear();
+
+			//loop over the primary vertices stored in the frame file
+			/*double* vertex_pos = new double[3];
+			for (unsigned int k = 0; k < root_frames[j]->GetVertex_x().size(); k++) {
+
+				TrackerHitImpl* frame_vertex = new TrackerHitImpl();
+
+				vertex_pos[0] = root_frames[j]->GetVertex_x()[k];
+				vertex_pos[1] = root_frames[j]->GetVertex_y()[k];
+				vertex_pos[2] = root_frames[j]->GetVertex_z()[k];
+
+				frame_vertex->setCellID0(300+j);
+				frame_vertex->setPosition(vertex_pos);
+
+				sensor_vertex_coll->addElement(frame_vertex);
+			}
+
+			delete [] vertex_pos;*/
 		}
+
+		/*TrackerHitImpl* test = new TrackerHitImpl();
+
+		test->setCellID0(hit_events[i]->hits.back()->sensor_ID);
+		test->setType(hit_events[i]->hits.back()->pdgId[hit_events[i]->hits.back()->pdgId.size()/2-1]);
+		test->setPosition(hit_events[i]->hits.back()->pos[hit_events[i]->hits.back()->pos.size()/2-1]);
+		test->setEDep(hit_events[i]->hits.back()->edep_total);
+
+		sensor_hit_coll->addElement(test);*/
 
 		event->addCollection(sensor_hit_coll, "true_hits_m26");
 		event->addCollection(sensor_frame_coll, "zsdata_m26");
+		//event->addCollection(sensor_vertex_coll, "MC_vertices_m26");
 
 		//write DUT true hit data collection if there are DUTs present
 		if (n_DUTs > 0) {
 
 			LCCollectionVec* DUT_hit_coll = new LCCollectionVec(LCIO::TRACKERHIT);
 			LCCollectionVec* DUT_frame_coll = new LCCollectionVec(LCIO::TRACKERDATA);
+			//LCCollectionVec* DUT_vertex_coll = new LCCollectionVec(LCIO::TRACKERHIT);
 			CellIDEncoder<TrackerDataImpl> frame_encoder_DUT(encoding_string, DUT_frame_coll);
 
 			for (int j = 0; j < n_DUTs; j++) {
 
 				TrackerDataImpl* frame_data = new TrackerDataImpl();
 
-				frame_trees[j+n_sensors]->SetBranchAddress("FramesData", &root_frame);
 				frame_trees[j+n_sensors]->GetEntry(i);
-				true_hit_trees[j+n_sensors]->SetBranchAddress("SimpleHits", &root_hit);
 
 				frame_encoder_DUT.reset();
 				frame_encoder_DUT["sensorID"] = j + 6;
 				frame_encoder_DUT["sparsePixelType"] = 2;
 				frame_encoder_DUT.setCellID(frame_data);
 
-				while (true_hit_trees[j+n_sensors]->GetEntry(DUT_hit_incrs[j]) and root_hit->run == i) {
+				while (true_hit_trees[j+n_sensors]->GetEntry(hit_incrs[j+n_sensors]) and root_hits[j+n_sensors]->run == i) {
+
+					/*hit_events[i]->hits.push_back(new TrueHit());
+					hit_events[i]->hits.back()->sensor_ID = DUT_IDs[j];
+					hit_events[i]->hits.back()->edep_total = root_hits[j]->edepTotal;
+					hit_events[i]->hits.back()->pdgId = root_hits[j]->pdgId;
+					hit_events[i]->hits.back()->edep = root_hits[j]->edep;*/
 
 					double* hit_pos = new double[3];
-					for (unsigned int k = 0; k < root_hit->pos.size(); k++) {
+					for (unsigned int k = 0; k < root_hits[j+n_sensors]->pos.size(); k++) {
 
-						TrackerHitImpl* true_hit_data = new TrackerHitImpl();
+						//hit_events[i]->hits.back()->pos.push_back(hit_pos);
 
-						hit_pos[0] = root_hit->pos[k].X();
-						hit_pos[1] = root_hit->pos[k].Y();
-						hit_pos[2] = root_hit->pos[k].Z();
+						if (k == root_hits[j+n_sensors]->pos.size()/2 - 1) {
 
-						true_hit_data->setCellID0(DUT_IDs[j]);
-						true_hit_data->setType(DUT_hit_incrs[j]);
-						true_hit_data->setPosition(hit_pos);
-						true_hit_data->setEDep(root_hit->edep[k]);
+							TrackerHitImpl* true_hit_data = new TrackerHitImpl();
 
-						DUT_hit_coll->addElement(true_hit_data);
+							hit_pos[0] = root_hits[j+n_sensors]->pos[k].X();
+							hit_pos[1] = root_hits[j+n_sensors]->pos[k].Y();
+							hit_pos[2] = root_hits[j+n_sensors]->pos[k].Z();
+
+							true_hit_data->setCellID0(DUT_IDs[j]);
+							true_hit_data->setQuality(root_hits[j+n_sensors]->event);
+							true_hit_data->setType(root_hits[j+n_sensors]->pdgId[k]);
+							true_hit_data->setPosition(hit_pos);
+							true_hit_data->setEDep(root_hits[j+n_sensors]->edepTotal);
+
+							DUT_hit_coll->addElement(true_hit_data);
+						}
 					}
 
-					DUT_hit_incrs[j]++;
+					hit_incrs[j+n_sensors]++;
 
 					delete [] hit_pos;
 				}
 
 				std::vector<float> charge_vec;
-				for (auto it = root_frame->GetHitMap().begin(); it != root_frame->GetHitMap().end(); ++it) {
+				for (auto it = root_frames[j]->GetHitMap().begin(); it != root_frames[j]->GetHitMap().end(); ++it) {
 
-					float x = (it->first)%root_frame->GetWidth();
-					float y = (it->first)/root_frame->GetWidth();
+					float x = (it->first)%root_frames[j]->GetWidth();
+					float y = (it->first)/root_frames[j]->GetWidth();
 					float TOT = it->second;
 
 					charge_vec.push_back(x);
@@ -304,10 +392,29 @@ int main(int argc, char* argv[]) {
 				frame_data->setChargeValues(charge_vec);
 				DUT_frame_coll->addElement(frame_data);
 				charge_vec.clear();
+
+
+				/*double* vertex_pos = new double[3];
+				for (unsigned int k = 0; k < root_frames[j]->GetVertex_x().size(); k++) {
+
+					TrackerHitImpl* frame_vertex = new TrackerHitImpl();
+
+					vertex_pos[0] = root_frames[j]->GetVertex_x()[k];
+					vertex_pos[1] = root_frames[j]->GetVertex_y()[k];
+					vertex_pos[2] = root_frames[j]->GetVertex_z()[k];
+
+					frame_vertex->setCellID0(DUT_IDs[j]);
+					frame_vertex->setPosition(vertex_pos);
+
+					DUT_vertex_coll->addElement(frame_vertex);
+				}
+
+				delete [] vertex_pos;*/
 			}
 
 			event->addCollection(DUT_hit_coll, "true_hits_DUT");
 			event->addCollection(DUT_frame_coll, "zsdata_DUT");
+			//event->addCollection(DUT_vertex_coll, "MC_vertices_DUT");
 		}
 
 
@@ -317,7 +424,7 @@ int main(int argc, char* argv[]) {
 		if (((i+1)%1000 == 0) || (i == 0)) std::cout << "succesfully recorded event " << i+1 << endl;
 	}
 
-	std::cout << "succesfully recorder all events\n";
+	std::cout << "succesfully recorded all events\n";
 
 	writer->flush();
 	writer->close();
@@ -329,13 +436,21 @@ int main(int argc, char* argv[]) {
 
 		delete true_hit_files[i];
 		delete frame_files[i];
+		delete root_hits[i];
+		delete root_frames[i];
 	}
+
+	/*for (int i = 0; i < n_runs; i++) {
+
+		delete hit_events[i];
+	}
+
+	delete [] hit_events;*/
 
 	delete [] true_hit_trees;
 	delete [] true_hit_files;
-
-	delete root_hit;
-	delete root_frame;
+	delete [] root_hits;
+	delete [] root_frames;
 
 	return 0;
 }
